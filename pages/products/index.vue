494626bb4558a8bee70bb103824b0185695040e9
RahulSoni <rahul.soni@kuliza.com>
2024-04-19 12:05:41 +0530

    initial commit

---
+++ pages/products/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1> products page </h1>
+    </div>
+</template>
+
+<script setup>
+  definePageMeta({
+    layout: "products"
+  })
+</script>
+
+<style scoped>
+</style>
